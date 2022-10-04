bf8a8b93a6dd3310cd05b3c9c3fa464fbb6a95d5
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-04 17:04:57 +0600

    update code

---
+++ Products.py
@@ -0,0 +1,47 @@
+import requests
+
+type_code = "P"
+print("starting data migration for " + type_code)
+base_url = "demo-eu.demo1.pricefx.com"
+partition = "demo_ark_solutions"
+url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
+
+payload = {
+  "endRow": 300,
+  "operationType": "fetch",
+  "startRow": 0,
+  "textMatchStyle": "exact"
+}
+
+headers = {"Content-Type": "application/json"}
+response = requests.post(url, json=payload, headers=headers, auth=('demo_ark_solutions/sm.hasan', 'smhasan123!'))
+data = response.json()
+print(data)
+
+
+
+# type_code = "P"
+# print("starting data migration for " + type_code)
+# base_url = "demo-eu.demo1.pricefx.com"
+# partition = "demo_ark_solutions"
+# url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
+#
+# payload = {
+#     "endRow": 300,
+#     "operationType": "fetch",
+#     "startRow": 0,
+#     "textMatchStyle": "exact"
+# }
+#
+# headers = {"Content-Type": "application/json"}
+# print(f"fetching all {type_code} from partition {partition}")
+# response = requests.post(url, json=payload, headers=headers, auth=('demo_ark_solutions/sm.hasan', 'smhasan123!'))
+# # // try catch exceptaion handeling
+# Products = response.json()["response"]["data"]
+# ProductIDs = []
+# for obj in Products:
+#     ProductIDs.append(obj["typedId"])
+#
+# print("Fetched typedIds" + str(ProductIDs))
+
+
