0149ac6e028266fa3608d844792bf03054132d22
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-06 11:10:47 +0600

    Fetch Price Record Data

---
+++ priceRecord.py
@@ -0,0 +1,22 @@
+
+import requests
+
+type_code = "PR"
+print("starting data migration for Type Code " + type_code)
+base_url = "demo-eu.demo1.pricefx.com"
+partition = "demo_ark_solutions"
+url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
+
+payload = {
+    "endRow": 300,
+    "operationType": "fetch",
+    "startRow": 0,
+    "textMatchStyle": "exact"
+}
+
+headers = {"Content-Type": "application/json"}
+print(f"fetching all {type_code} from partition {partition}")
+response = requests.post(url, json=payload, headers=headers, auth=('demo_ark_solutions/sm.hasan', 'smhasan123!'))
+# // try catch exceptaion handeling
+priceRecords = response.json()["response"]["data"]
+print(priceRecords)
