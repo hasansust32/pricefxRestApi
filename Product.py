408c0f43f260014c5f9a147fe81c34d908e1b6db
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-11 11:45:59 +0600

    Product table update for PFX

--- Product.py
+++ Product.py
@@ -2,8 +2,8 @@ import requests
 
 type_code = "P"
 print("starting data migration for Type Code " + type_code)
-base_url = "demo-eu.demo1.pricefx.com"
-partition = "demo_ark_solutions"
+base_url = "fbu-qa.pricefx.eu"
+partition = "iplex-dev"
 url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
 
 payload = {
@@ -14,34 +14,12 @@ payload = {
 }
 
 headers = {"Content-Type": "application/json"}
-response = requests.post(url, json=payload, headers=headers, auth=('demo_ark_solutions/sm.hasan', 'smhasan123!'))
+response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
 data = response.json()
 print(data)
 
 
 
-# type_code = "P"
-# print("starting data migration for " + type_code)
-# base_url = "demo-eu.demo1.pricefx.com"
-# partition = "demo_ark_solutions"
-# url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
-#
-# payload = {
-#     "endRow": 300,
-#     "operationType": "fetch",
-#     "startRow": 0,
-#     "textMatchStyle": "exact"
-# }
-#
-# headers = {"Content-Type": "application/json"}
-# print(f"fetching all {type_code} from partition {partition}")
-# response = requests.post(url, json=payload, headers=headers, auth=('demo_ark_solutions/sm.hasan', 'smhasan123!'))
-# # // try catch exceptaion handeling
-# Products = response.json()["response"]["data"]
-# ProductIDs = []
-# for obj in Products:
-#     ProductIDs.append(obj["typedId"])
-#
-# print("Fetched typedIds" + str(ProductIDs))
+
 
 
