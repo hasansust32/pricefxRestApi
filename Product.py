dbd6d2171ddc0816479a9e8671335f89d7b35aac
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-19 06:16:32 +0600

    product fetch

--- Product.py
+++ Product.py
@@ -1,3 +1,27 @@
+# import requests
+#
+# type_code = "P"
+# print("starting data migration for Type Code " + type_code)
+# base_url = "fbu-qa.pricefx.eu"
+# partition = "iplex-dev"
+# url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
+#
+# payload = {
+#   "endRow": 5200,
+#   "operationType": "fetch",
+#   "startRow": 0,
+#   "textMatchStyle": "exact"
+# }
+#
+# headers = {"Content-Type": "application/json"}
+# response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
+# data = response.json()["response"]["data"]
+# print(data)
+#
+#
+
+
+
 import requests
 
 type_code = "P"
@@ -7,19 +31,15 @@ partition = "iplex-dev"
 url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
 
 payload = {
-  "endRow": 5200,
+  "endRow": 5500,
   "operationType": "fetch",
   "startRow": 0,
   "textMatchStyle": "exact"
 }
 
 headers = {"Content-Type": "application/json"}
+
 response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
+
 data = response.json()["response"]["data"]
 print(data)
-
-
-
-
-
-
