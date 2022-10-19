c35340d6ba31beaed164599faa46030b4051979b
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-19 06:16:47 +0600

    price Record Update

--- PriceRecordUpdate.py
+++ PriceRecordUpdate.py
@@ -3,40 +3,40 @@ import sys
 # Saving the reference of the standard output
 original_stdout = sys.stdout
 
-with open('01.UpdatePriceRecord.txt', 'a') as f:
+with open('Hasan_PriceRecord_Update1.txt', 'a') as f:
     sys.stdout = f
 
     import requests
     def upsertData(payload):
-      if "sku" in payload:
-        type_code = "PR"
-        base_url = "fbu-qa.pricefx.eu"
-        partition = "iplex-dev"
-        url = "https://" + base_url + "/pricefx/" + partition + "/integrate/" + type_code
+        if "sku" in payload:
+            type_code = "PR"
+            base_url = "fbu-qa.pricefx.eu"
+            partition = "iplex-dev"
+            url = "https://" + base_url + "/pricefx/" + partition + "/integrate/" + type_code
 
-        payload = {
-          "data": {
-            "typedId": payload["typedId"],
-            "sku": payload["sku"],
-            "customerId": payload["customerId"]
-          }
-        }
+            payload = {
+                "data": {
+                    "typedId": payload["typedId"],
+                    "sku": payload["sku"],
+                    "customerId": payload["customerId"]
+                }
+            }
+
+            headers = {"Content-Type": "application/json"}
+            print(f"Object update request payload {payload}")
+            response = requests.post(url, json=payload, headers=headers,
+                                     auth=('iplex-dev/sm.hasan', 'start123'))
+            # print response statu
+            data = response.json()
+            print(data)
 
 
-
-        headers = {"Content-Type": "application/json"}
-        print(f"Object update request payload {payload}")
-        response = requests.post(url, json=payload, headers=headers,
-                                 auth=('iplex-dev/sm.hasan', 'start123'))
-        # print response statu
-        data = response.json()
-        print(data)
 
 
     def ProductMigration():
         import csv
 
-        with open("DataSet/ProductMappingData.csv", 'r') as file1:
+        with open("DataSet/UpdatedProductMapping.csv", 'r') as file1:
             csvreader = csv.reader(file1)
             Migration1 = {
 
@@ -66,47 +66,45 @@ with open('01.UpdatePriceRecord.txt', 'a') as f:
 
 
     def updatePriceRecord(idList):
-      for id in idList:
-        myID = id.split(".")[0]
-
         ProductTranslationMap = ProductMigration()
         CustomerTranslationMap = CustomerMigration()
 
+        for id in idList:
+            myID = id.split(".")[0]
 
+            type_code = "PR"
+            base_url = "fbu-qa.pricefx.eu"
+            partition = "iplex-dev"
+            url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code + "/" + myID
 
-        type_code = "PR"
-        base_url = "fbu-qa.pricefx.eu"
-        partition = "iplex-dev"
-        url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code + "/" + myID
+            print(f"Fetching object {myID}")
 
-        print(f"Fetching object {myID}")
+            response = requests.post(url, auth=('iplex-dev/sm.hasan', 'start123'))
 
-        response = requests.post(url, auth=('iplex-dev/sm.hasan', 'start123'))
+            priceRecord = response.json()["response"]["data"][0]
+            print(f"Fetched object {priceRecord}")
 
-        priceRecord = response.json()["response"]["data"][0]
-        print(f"Fetched object {priceRecord}")
+            if "sku" in priceRecord:
+                currentSku = priceRecord["sku"]
 
-        if "sku" in priceRecord:
-          currentSku = priceRecord["sku"]
+            if currentSku in ProductTranslationMap:
+                priceRecord["sku"] = ProductTranslationMap[currentSku]
 
-        if currentSku in ProductTranslationMap:
-          priceRecord["sku"] = ProductTranslationMap[currentSku]
+            if "customerId" in priceRecord:
+                currentCustomerId = priceRecord["customerId"]
 
-        if "customerId" in priceRecord:
-          currentCustomerId = priceRecord["customerId"]
+            if currentCustomerId in CustomerTranslationMap:
+                priceRecord["customerId"] = CustomerTranslationMap[currentCustomerId]
 
-        if currentCustomerId in CustomerTranslationMap:
-          priceRecord["customerId"] = CustomerTranslationMap[currentCustomerId]
+            print("Updated object " + str(priceRecord))
 
-        print("Updated object " + str(priceRecord))
+            upsertData(priceRecord)
 
-
-
-        upsertData(priceRecord)
 
 
     type_code = "PR"
     print(f"#-----------------------------------------------------------------------------------------------------------------------------------------------------")
+
     print("starting data migration for Type Code " + type_code)
     base_url = "fbu-qa.pricefx.eu"
     partition = "iplex-dev"
@@ -140,9 +138,7 @@ with open('01.UpdatePriceRecord.txt', 'a') as f:
     updatePriceRecord(CustomerIDs)
 
 
-    # print('Hello, Python!')
-    # print('This message will be written to a file.')
-    # # Reset the standard output
+
     sys.stdout = original_stdout
 
 
