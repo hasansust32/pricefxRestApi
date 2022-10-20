fc5e37db293874cbcdff1d9e2a7b3d45ab7acbd2
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-20 15:56:47 +0600

    Quote File Creation

--- Quote.py
+++ Quote.py
@@ -1,34 +1,44 @@
-import requests
 
-base_url = "demo-eu.demo1.pricefx.com"
-partition = "demo_ark_solutions"
+import sys
 
-url = "https://" + base_url + "/pricefx/" + partition + "/quotemanager.fetchlist"
+# Saving the reference of the standard output
+original_stdout = sys.stdout
 
-payload = {
-  "endRow": 300,
-  "oldValues": None,
-  "operationType": "fetch",
-  "startRow": 0,
-  "textMatchStyle": "exact",
-  "data": {
-    "_constructor": "AdvancedCriteria",
-    "operator": "and",
-    "criteria": [
-      {
-        "fieldName": "quoteStatus",
-        "operator": "equals",
-        "value": "DEAL"
+with open('01.Quote_Report.txt', 'a') as f:
+    sys.stdout = f
+
+    import requests
+
+    base_url = "fbu-qa.pricefx.eu"
+    partition = "iplex-dev"
+
+    url = "https://" + base_url + "/pricefx/" + partition + "/quotemanager.fetchlist"
+
+    payload = {
+      "endRow": 300,
+      "oldValues": None,
+      "operationType": "fetch",
+      "startRow": 0,
+      "textMatchStyle": "exact",
+      "data": {
+        "_constructor": "AdvancedCriteria",
+        "operator": "and",
+        "criteria": [
+          {
+            "fieldName": "quoteStatus",
+            "operator": "equals",
+            "value": "DEAL"
+          }
+        ]
       }
-    ]
-  }
-}
+    }
 
-headers = {"Content-Type": "application/json"}
+    headers = {"Content-Type": "application/json"}
 
-response = requests.post(url, json=payload, headers=headers, auth=('demo_ark_solutions/sm.hasan', 'smhasan123!'))
+    response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
 
-data = response.json()["response"]["data"]
+    data = response.json()["response"]["data"]
+
+    print(data)
 
 
-print(data)
