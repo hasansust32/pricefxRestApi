9e8b7308061a68f9318edd2e2aba4c2b1ce31e95
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-21 20:54:51 +0600

    product code

--- Product.py
+++ Product.py
@@ -1,25 +1,3 @@
-# import requests
-#
-# type_code = "P"
-# print("starting data migration for Type Code " + type_code)
-# base_url = "fbu-qa.pricefx.eu"
-# partition = "iplex-dev"
-# url = "https://" + base_url + "/pricefx/" + partition + "/fetch/" + type_code
-#
-# payload = {
-#   "endRow": 5200,
-#   "operationType": "fetch",
-#   "startRow": 0,
-#   "textMatchStyle": "exact"
-# }
-#
-# headers = {"Content-Type": "application/json"}
-# response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
-# data = response.json()["response"]["data"]
-# print(data)
-#
-#
-
 
 
 import requests
