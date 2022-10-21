665db0591d98bdf84f4d29b69e63a251dfa21809
S. M Mahamudul Hasan <67460226+hasansust32@users.noreply.github.com>
2022-10-21 20:53:47 +0600

    Get Quote code

--- Quote.py
+++ Quote.py
@@ -1,44 +1,54 @@
 
-import sys
 
-# Saving the reference of the standard output
-original_stdout = sys.stdout
+import requests
 
-with open('01.Quote_Report.txt', 'a') as f:
-    sys.stdout = f
+typed_id = "24.Q"
+base_url = "fbu-qa.pricefx.eu"
+partition = "iplex-dev"
 
-    import requests
+url = "https://" + base_url + "/pricefx/" + partition + "/quotemanager.fetchproducts"
 
-    base_url = "fbu-qa.pricefx.eu"
-    partition = "iplex-dev"
 
-    url = "https://" + base_url + "/pricefx/" + partition + "/quotemanager.fetchlist"
+url2 = "https://" + base_url + "/pricefx/" + partition + "/quotemanager.fetch/" + typed_id
 
-    payload = {
-      "endRow": 300,
-      "oldValues": None,
-      "operationType": "fetch",
-      "startRow": 0,
-      "textMatchStyle": "exact",
-      "data": {
-        "_constructor": "AdvancedCriteria",
-        "operator": "and",
-        "criteria": [
-          {
-            "fieldName": "quoteStatus",
-            "operator": "equals",
-            "value": "DEAL"
-          }
-        ]
-      }
+response2 = requests.get(url2, auth=('iplex-dev/sm.hasan', 'start123'))
+data = response2.json()['response']['data']
+# print(data)
+
+
+
+payload = {
+  "operationType": "fetch",
+  "startRow": 0,
+  "textMatchStyle": "exact",
+  "oldValues": None,
+  "distinctResults": False,
+  "data": {
+    "quote": data[0],
+
+    "filterCriteria": {
+      "_constructor": "AdvancedCriteria",
+      "operator": "and",
+      "criteria": [
+        {
+          "fieldName": "",
+          "operator": "",
+          "value": ""
+        }
+      ]
     }
+  },
+  "endRow": 600,
+  "sortBy": [
+    "sku"
+  ]
+}
 
-    headers = {"Content-Type": "application/json"}
+headers = {"Content-Type": "application/json"}
 
-    response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
+response = requests.post(url, json=payload, headers=headers, auth=('iplex-dev/sm.hasan', 'start123'))
 
-    data = response.json()["response"]["data"]
-
-    print(data)
+data = response.json()['response']['data']
+print(data)
 
 
